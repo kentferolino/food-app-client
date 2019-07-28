2750105b5445b98cc3d7d06d6e242c65872d724c
Kent <kentfero@gmail.com>
2019-07-28 08:06:45 +0800

    Add icons to side nav.

--- src/utils/route.js
+++ src/utils/route.js
@@ -1,27 +1,54 @@
-import { Build, Home as HomeIcon } from '@material-ui/icons';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faIceCream,
+  faMugHot,
+  faCheese,
+  faCarrot,
+  faFish,
+  faHome
+} from '@fortawesome/free-solid-svg-icons';
 import Home from '../pages/Home';
 import ChangePW from '../pages/ChangePW';
 import UserProfile from '../pages/UserProfile';
+
 
 export const navRoutes = [
   {
     path: '/',
     main: () => <h2>Home</h2>,
     label: 'Home',
-    icon: () => <HomeIcon />
+    icon: () => <FontAwesomeIcon icon={faHome} size="2x" />
   },
   {
     path: '/soup',
     main: () => <h2>Soup</h2>,
     label: 'Soup',
-    icon: () => <Build />
+    icon: () => <FontAwesomeIcon icon={faMugHot} size="2x" />
   },
   {
-    path: '/shoelaces',
-    main: () => <h2>Shoelaces</h2>,
-    label: 'Shoelaces',
-    icon: () => <Build />
+    path: '/appetizer',
+    main: () => <h2>Appetizer</h2>,
+    label: 'Appetizer',
+    icon: () => <FontAwesomeIcon icon={faCheese} size="2x" />
+  },
+  {
+    path: '/sidedish',
+    main: () => <h2>Side dish</h2>,
+    label: 'Side dish',
+    icon: () => <FontAwesomeIcon icon={faCarrot} size="2x" />
+  },
+  {
+    path: '/maincourse',
+    main: () => <h2>Main Course</h2>,
+    label: 'Main course',
+    icon: () => <FontAwesomeIcon icon={faFish} size="2x" />
+  },
+  {
+    path: '/dessert',
+    main: () => <h2>Dessert</h2>,
+    label: 'Dessert',
+    icon: () => <FontAwesomeIcon icon={faIceCream} size="2x" />
   },
 ];
 
